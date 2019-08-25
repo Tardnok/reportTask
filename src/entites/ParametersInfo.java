811d0c486ba43cf9848c1210ce59625daf31136b
DaryaKondrat <darya.kondrat1997@gmail.com>
2019-08-25 14:36:33 +0300

    создан пакет класс ParametersInfo

---
+++ src/entites/ParametersInfo.java
@@ -0,0 +1,19 @@
+package entites;
+
+public class ParametersInfo {
+    private int width;
+    private int height;
+
+    public ParametersInfo(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
