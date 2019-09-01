2359498911d2569694ff8e3d6b17ad68a7f5f7df
DaryaKondrat <darya.kondrat1997@gmail.com>
2019-09-01 18:07:59 +0300

    константы вынесены в поля класса,переделана логика выведения строк в файл

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ import static java.util.Arrays.asList;
 public class Main {
     private static String filePath = "E:\\Work\\reportTask\\src\\data\\source-data.txt";
     private static String fileReport = "E:\\Work\\reportTask\\src\\data\\report.txt";
+    private static final String HEADER = "| Номер  | Дата  | ФИО   |";
+    private static final String SEPARATING_LINE = "--------------------------";
+    private static final String SEPARATING_TILDE = "~";
 
     public static void main(String[] args) {
         List<StaffMemberInfo> staff = csvParser(filePath);
@@ -67,32 +70,83 @@ public class Main {
     }
 
     public static List<String> printToFile(List<StaffMemberInfo> staffMembers, ParametersInfo parametersInfo) {
-        String header = "| Номер  | Дата  | ФИО   |";
-        String separatingline = "--------------------------";
-        String separatingTilde = "~";
         List<String> info = new ArrayList<>();
-        info.add(header);
+        info.add(HEADER);
+        info.add(SEPARATING_LINE);
         for (StaffMemberInfo staff : staffMembers) {
             int countLine = 1;
+            int countOfGap = 0;
+            List<String> stringsForNumber = new ArrayList<>();
+            List<String> stringsForDate = new ArrayList<>();
+            List<String> stringsForName = new ArrayList<>();
             if (staff.getDate().length() > parametersInfo.getDateWidth()) {
-                countLine = countLines(staff.getDate().length(), parametersInfo.getDateWidth());
+                stringsForDate = returnMassive(staff.getDate(), parametersInfo.getDateWidth());
             }
-            for (int i = 0; i < countLine; i += 7) {
-                info.add("| " + staff.getNumber() + "  | " + staff.getDate().substring(i) + " | " + staff.getFullName() + " |\n");
+            if (staff.getFullName().length() >= parametersInfo.getNameWidth()) {
+                System.out.println(staff.getFullName());
+                stringsForName = returnMassive(staff.getFullName(), parametersInfo.getNameWidth());
             }
-            info.add(separatingline);
+            if (staff.getNumber().length() >= parametersInfo.getNumberWidth()) {
+                stringsForNumber = returnMassive(staff.getFullName(), parametersInfo.getNumberWidth());
+            }
+            if (stringsForDate.size() > stringsForName.size()) {
+                countLine = stringsForDate.size();
+                countOfGap = stringsForDate.size() - stringsForName.size();
+                if (countOfGap != 0) {
+                    for (int i = 0; i < countOfGap; i++) {
+                        stringsForName.add(" ");
+                    }
+                    for (int i = 0; i < countLine; i++) {
+                        stringsForNumber.add(" ");
+                    }
+                }
+            } else {
+                countLine = stringsForName.size();
+                countOfGap = stringsForName.size() - stringsForDate.size();
+                if (countOfGap != 0) {
+                    for (int i = 0; i < countOfGap; i++) {
+                        stringsForDate.add(" ");
+
+                    }
+                    for (int i = 0; i < countLine; i++) {
+                        stringsForNumber.add(" ");
+                    }
+                }
+            }
+            System.out.println(staff.getFullName());
+            for (int i = 0; i < countLine; i ++) {
+                info.add("| " + stringsForNumber.get(i) + "  | "
+                        + stringsForDate.get(i) + " | "
+                        + stringsForName.get(i) + " |\n");
+            }
+            info.add(SEPARATING_LINE);
         }
-        info.add(separatingTilde);
+        info.add(SEPARATING_TILDE);
         return info;
     }
 
-    public static int countLines(int staffLenght, int parameterLenght) {
-        int countLine = 2;
-        int length = staffLenght - parameterLenght;
-        while (length > parameterLenght) {
-            length = length - parameterLenght;
-            countLine++;
+
+    public static List<String> returnMassive(String field, int parameterLentgh) {
+        List<String> arr = new ArrayList<>();
+        int initialIndex = 0;
+        int length = field.length();
+        while (length > parameterLentgh) {
+            length = length - parameterLentgh;
+            arr.add("| " + field.substring(initialIndex, parameterLentgh) + " |");
+            initialIndex = parameterLentgh;
         }
-        return countLine;
+        if (length != 0) {
+            String str = field.substring(initialIndex);
+            String lastLine = "| " + str + " |";
+            int remainderLength = parameterLentgh - str.length();
+            if (remainderLength != 0) {
+                while (remainderLength > 0) {
+                    str += " ";
+                    remainderLength --;
+                }
+            }
+            arr.add(lastLine);
+        }
+        return arr;
     }
 }
